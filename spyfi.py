faa5509dd649f609252e688a86fa6f0d0e1abe95
ac3965 <ac3965@cs.drexel.edu>
2020-04-15 12:19:46 -0400

    Added main script

---
+++ spyfi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env
+#Author: Ansh - mrdebator
+import os
+
+print('Python Script to automate network monitoring and estimate geographic location of Access Points.')
+os.system('sudo airmon-ng')
+
+interfaceName = input('Choose which interface to commence monitor mode: ')
+
+#print('Some processes must be terminated for monitor mode: ')
+checkProcessesStream = os.popen('sudo airmon-ng check')
+checkProcesses = checkProcessesStream.read()
+
+if(checkProcesses):
+	print('Some processes must be terminated for monitor mode: ')
+	print(checkProcesses)
+
+print('Running `check kill`...')
+os.system('sudo airmon-ng check kill')
+
+#start monitor mode on selected interface
+
+monitorModeCommand = "sudo airmon-ng start " + interfaceName
+os.system(monitorModeCommand)
+
+print('Beginning monitor mode...')
+os.system('sudo airodump-ng '+interfaceName)
+
+
+
